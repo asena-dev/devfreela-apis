4a03640f3dd4b5041e89589029bae4209825744b
Adenilson Sena <adenilson@tuenda.ao>
2022-09-10 13:15:08 -0300

    termino do modulo Desenvolvimento de APIs com ASP.NET Core

--- DevFreela.API/Controllers/ProjectsController.cs
+++ DevFreela.API/Controllers/ProjectsController.cs
@@ -1,14 +1,79 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DevFreela.API.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
 
 namespace DevFreela.API.Controllers
 {
     [Route("api/projects")]
     public class ProjectsController : ControllerBase
     {
+        private readonly OpeningTimeOption _option;
+        public ProjectsController(IOptions<OpeningTimeOption> option)
+        {
+            _option = option.Value;
+        }
+        // api/projects?query=net core
         [HttpGet]
-        public IActionResult Get()
+        public IActionResult Get(string query)
         {
             return Ok();
+        }
+        // api/projects/599
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            //return NotFound();
+
+            return Ok();
+        }
+        [HttpPost]
+        public IActionResult Post([FromBody] CreateProjectModel createProject)
+        {
+            if (createProject.Title.Length > 50)
+            {
+                return BadRequest();
+            }
+
+            return CreatedAtAction(nameof(GetById), new { id = createProject.Id }, createProject);
+        }
+        // api/projects/1
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] UpdateProjectModel updateProject)
+        {
+            if (updateProject.Description.Length > 200)
+            {
+                return BadRequest();
+            }
+
+            return NoContent();
+        }
+        // api/prolects/3
+        [HttpDelete]
+        public IActionResult Delete(int id)
+        {
+            // Buscar, se não existir, retorna NotFound
+
+            // remover
+
+            return NoContent();
+        }
+        // api/projetcs/1/comments
+        [HttpPost("{id}/comments")]
+        public IActionResult PostComment(int id, [FromBody] CreateCommentModel createComment)
+        {
+            return NoContent();
+        }
+        // api/projetcs/1/start
+        [HttpPut("{id}/start")]
+        public IActionResult Start(int id)
+        {
+            return NoContent();
+        }
+        // api/projetcs/finish
+        [HttpPut("{id}/finish")]
+        public IActionResult Finish(int id)
+        {
+            return NoContent();
         }
     }
 }
--- DevFreela.API/Program.cs
+++ DevFreela.API/Program.cs
@@ -1,11 +1,21 @@
+using DevFreela.API.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.Configure<OpeningTimeOption>(builder.Configuration.GetSection("OpeningTime"));
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//services cors
+builder.Services.AddCors(p => p.AddPolicy("corsapp", builder =>
+{
+    builder.WithOrigins("*").AllowAnyMethod().AllowAnyHeader();
+}));
 
 var app = builder.Build();
 
@@ -15,6 +25,9 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+//app cors
+app.UseCors("corsapp");
 
 app.UseHttpsRedirection();
 
